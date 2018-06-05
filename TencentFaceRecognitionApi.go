bac62f981014bae08543032b38fff94b2cf6350c
rbqrbq <domdou@tsinkia.com>
2018-06-05 18:56:06 +0800

    public

--- TencentFaceRecognitionApi.go
+++ TencentFaceRecognitionApi.go
@@ -1,4 +1,4 @@
-package TencentApi
+package TencentFaceRecognitionApi
 
 import (
 	"log"
@@ -13,12 +13,14 @@ import (
 	"encoding/base64"
 )
 
+
+
 type TencentAPI struct{
-	appid string
-	mode int
-	sessionId string
-	sessionKey string
-	url string
+	Appid string
+	Mode int
+	SessionId string
+	SessionKey string
+	Url string
 }
 /**
 * 通过图片url获取==调用api
@@ -30,28 +32,13 @@ type TencentAPI struct{
 * url 请求路径
 */
 func (tAPI TencentAPI) PostByUrl(imageUrl string) string{
-	t := time.Now()
-	currentUnix := t.Unix()
-	resource := rand.NewSource(currentUnix)
-	sourceRand  := rand.New(resource)
+	
 
 	//json序列化
-	postData := fmt.Sprintf("{\"appid\":\"%s\",\"mode\":%d,\"url\":\"%s\"}",tAPI.appid,tAPI.mode,imageUrl)
+	postData := fmt.Sprintf("{\"appid\":\"%s\",\"mode\":%d,\"url\":\"%s\"}",tAPI.Appid,tAPI.Mode,imageUrl)
 	
-	
-	srcStr:=fmt.Sprintf("a=%s&k=%s&e=%d&t=%d&r=%d&u=0&f=",tAPI.appid,tAPI.sessionId,currentUnix+2400,currentUnix,sourceRand.Intn(999999999))
-	
-	
-	key := []byte(tAPI.sessionKey)
-	hashHmac := hashHmac(srcStr,key)
-	result := bytesCombine(hashHmac,[]byte(srcStr))
-	
-	encodeString := base64.StdEncoding.EncodeToString(result)
-	
-	fmt.Println(encodeString)
-	
-	
-	request , err  := http.NewRequest("POST",tAPI.url,bytes.NewBuffer([]byte(postData)))
+	encodeString := tAPI.sign()
+	request , err  := http.NewRequest("POST",tAPI.Url,bytes.NewBuffer([]byte(postData)))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -60,16 +47,52 @@ func (tAPI TencentAPI) PostByUrl(imageUrl string) string{
 	request.Header.Set("authorization",	encodeString)
 	client := &http.Client{}
 	resp , err := client.Do(request)
-	defer resp.Body.Close()
 	if err != nil {
 		log.Panic(err)
 	}
+	defer resp.Body.Close()
 	respBytes , err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Panic(err)
 	}
 	return string(respBytes)
 }
+
+
+func (tAPI TencentAPI) postByFile(filePath string) string{
+	fileByte , err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Panic(err)
+	}
+	//json序列化
+	fmt.Println(fileByte)
+	postData := fmt.Sprintf("{\"appid\":\"%s\",\"mode\":%d,\"image\":\"%v\"}",tAPI.Appid,tAPI.Mode,fileByte)
+	fmt.Println(postData)
+	
+	encodeString := tAPI.sign()
+
+
+	request , err  := http.NewRequest("POST",tAPI.Url,bytes.NewBuffer([]byte(postData)))
+	if err != nil {
+		log.Panic(err)
+	}
+	request.Header.Set("Content-Type","multipart/form-data")
+	request.Header.Set("host","recognition.image.myqcloud.com")
+	request.Header.Set("authorization",	encodeString)
+	client := &http.Client{}
+	resp , err := client.Do(request)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer resp.Body.Close()
+	fmt.Println(encodeString)	
+	respBytes , err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(respBytes)
+}
+
 
 func bytesCombine(pBytes ...[]byte) []byte{
 	return bytes.Join(pBytes,[]byte(""))
@@ -79,4 +102,21 @@ func hashHmac(srcStr string,key []byte) []byte{
 	mac := hmac.New(sha1.New,key)
 	mac.Write([]byte(srcStr))
 	return mac.Sum(nil)
+}
+
+func (tAPI TencentAPI) sign() string{
+	t := time.Now()
+	currentUnix := t.Unix()
+	resource := rand.NewSource(currentUnix)
+	sourceRand  := rand.New(resource)
+
+	srcStr:=fmt.Sprintf("a=%s&k=%s&e=%d&t=%d&r=%d&u=0&f=",tAPI.Appid,tAPI.SessionId,currentUnix+10,currentUnix,sourceRand.Intn(999999999))
+	
+	
+	key := []byte(tAPI.SessionKey)
+	hashHmac := hashHmac(srcStr,key)
+	result := bytesCombine(hashHmac,[]byte(srcStr))
+	
+	encodeString := base64.StdEncoding.EncodeToString(result)
+	return  encodeString
 }
